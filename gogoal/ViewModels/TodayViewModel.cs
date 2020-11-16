63f51860d9630ca344cd664a1a25eb7f6e5ba36c
Rosa Luo <portannaluo@hotmail.com>
2020-11-17 00:57:22 +1100

    1. Relocate View, Model, ViewModel files into folders 2. Add ExtendedBindableObject, provides compile-time type safety and refactoring support when renaming properties 3. Add ViewModel for Today page, doesn't include Command part

--- gogoal/ViewModels/TodayViewModel.cs
+++ gogoal/ViewModels/TodayViewModel.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using System.Collections.ObjectModel;
 using System.Collections.Generic;
-namespace gogoal
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+namespace gogoal.ViewModels
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Today : ContentPage
+    public class TodayViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<ToDoItemGroupedModel> ToDoItemsGrouped { get; } = new ObservableCollection<ToDoItemGroupedModel>();
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public DateTime DateToday { get { return DateTime.Now; } }
 
         public DateTime SelectedDate { get; set; }
 
-        public Today()
-        {
-            InitializeComponent();
-            SelectedDate = DateToday;
-            InitializeData();
-        }
+        public ObservableCollection<ToDoItemGroupedModel> ToDoItemsGrouped { get; }
+            = new ObservableCollection<ToDoItemGroupedModel>();
 
-        public Today(DateTime dateTime)
+
+        public TodayViewModel(DateTime dateTime)
         {
-            InitializeComponent();
             SelectedDate = dateTime;
             InitializeData();
         }
 
         async void InitializeData()
         {
+            //TODO Need to Group todoList fetched from database
             List<ToDoItemModel> todoList = await App.Database.GetItemsAsync();
             var goalTodoItems = new ToDoItemGroupedModel("Goal", "G");
             var generalToDoItems = new ToDoItemGroupedModel("General", "GNR");
@@ -79,45 +74,11 @@ namespace gogoal
                 .Build());
             ToDoItemsGrouped.Add(goalTodoItems);
             ToDoItemsGrouped.Add(generalToDoItems);
-            TodayTodoList.ItemsSource = ToDoItemsGrouped;
-        }
-        
-        void DatePicker_DateSelected(System.Object sender, DateChangedEventArgs e)
-        {
-            //TODO to show todo items for the choosen date
-            //不刷新页面，只刷新数据，同时控制“Today的显示”
-            if(e.NewDate == e.OldDate)
-            {
-                return;
-            }
         }
 
-        async void TodoItemEntry_Completed(System.Object sender, System.EventArgs e)
+        protected void OnPropertyChanged(string propertyName)
         {
-            if (String.IsNullOrEmpty(((Entry)sender).Text))
-                return;
-            ToDoItemsGrouped[1].Add(
-                new ToDoItemModel.Builder(Guid.NewGuid(), ((Entry)sender).Text)
-                .WithColor(null)
-                .WithDetails(null)
-                .WithStartDate(DateTime.Today.AddDays(1))
-                .WithGoalId(null)
-                .WithImportantLevel(ImportantLevelEnumeration.Undefined)
-                .WithIsChecked(false)
-                .Build());
-
-            //Add data into local database
-            await App.Database.InsertItemAsync(new ToDoItemModel.Builder(Guid.NewGuid(), ((Entry)sender).Text)
-                .WithColor(null)
-                .WithDetails(null)
-                .WithStartDate(DateTime.Today.AddDays(1))
-                .WithGoalId(null)
-                .WithImportantLevel(ImportantLevelEnumeration.Undefined)
-                .WithIsChecked(false)
-                .Build());
-
-            //Set Entry text as null again
-            ((Entry)sender).Text = null;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
